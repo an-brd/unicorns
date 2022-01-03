d51ff3198f019949204b224a906df93e02211c30
an-brd <94487869+an-brd@users.noreply.github.com>
2022-01-03 15:08:53 +0000

    some more changes

--- script.js
+++ script.js
@@ -1,32 +1,56 @@
 "use strict"
 
+let unicorns = []
+let numbUnicorns = 10
 
-let uni0=document.getElementById("unicorn0")
-let uni0x=0  //Unicorn0's X position
+// calls the setup function, to set up all the unicorns
+setup()
 
+//Start the race
+requestAnimationFrame(moveUnicorns)
 
-//Extra challenges .. Set the background colour of each unicorn .. (so they are different) .. ideally with Javascript (not CSS)
-//Add a third unicorn - three horse race
-// - What if we wanted 20 unicorns ??  []
+function setup () {
+	let i = 0 
+	while (i < numbUnicorns) {
+		// create unicorns
 
+		let u = {}
+		u.div = document.createElement("div")
+		document.body.appendChild(u.div)
+		u.div.classList.add("unicorn")
+		u.div.style.top = 1+ i * 10 + "%"
 
-setInterval(moveUnicorns,100)  //How fast the race is - smaller=faster
+		u.x = 0
+		let colours="red,blue,green,orange,purple,white,pink,grey,violet,turquoise".split(",")
+		u.div.style.backgroundColor = colours[i]
+		unicorns[i] = u
 
-
-function moveUnicorns(){
-
-    uni0.style.left= uni0x +"%"
-
-    uni0x +=1  //add 1% to the unicorns x position (Add a random amount instead)
-        
-    //Move all unicorns a random amount each
-    //If a unicorn has crossed the finish line
-        //Display winner
-        // restart race           
-
+		i++
+	}
 }
 
+function resetRace() {
+	let i = 0
+	while (i < numbUnicorns) {
+		unicorns[i].x = 0
+		i++
+	}
+}
 
+function moveUnicorns() {
+	
+	let i = 0
+	while (i<numbUnicorns) {
+		unicorns[i].div.style.left = unicorns[i].x + "%"
+		unicorns[i].x += Math.random()
 
+		let colours="red,blue,green,orange,purple,white,pink,grey,violet,turquoise".split(",")
 
-
+		if (unicorns[i].x > 90){
+			alert (`${colours[i]} unicorn wins the race`)
+			resetRace()
+		}
+		i++
+	}
+	requestAnimationFrame(moveUnicorns)
+}
